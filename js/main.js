86e3bb7480266a2004fe5970ed4561ee68dcccf6
tima1702 <tima1702@gmail.com>
2017-06-27 10:59:16 +0300

    Регистрация

--- js/main.js
+++ js/main.js
@@ -1,18 +1,33 @@
-function registration(){
-    var obj = getObjectRegistration();
-    console.log(obj);
-    //return false;
-    if(document.getElementById(inputPassword).value != document.getElementById(confirmPassword).value){
+//hash
+String.prototype.hashCode = function(){
+    if (Array.prototype.reduce){
+        return this.split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0);
+    }
+    var hash = 0;
+    if (this.length === 0) return hash;
+    for (var i = 0; i < this.length; i++) {
+        var character  = this.charCodeAt(i);
+        hash  = ((hash<<5)-hash)+character;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
 
-    };
+
+function registration(form){
+
+    if(!validateRegistration()) return false;
+    var obj=getObjectRegistration();
     var serialObj =JSON.stringify(obj);
-    console.log(serialObj);
+    //console.log(serialObj);
     if(!(localStorage.getItem(obj.login) == null || localStorage.getItem(obj.login) == undefined)) {
         alert("Пользователь с таким логином зарегистрировался в системе");
         return false;
     }
     localStorage.setItem(obj.login,serialObj);
 
+    form.reset();
+    alert("Вы зарегистрированны в системе!");
 }
 
 function getObjectRegistration(){
@@ -21,9 +36,35 @@ function getObjectRegistration(){
 
     object.login = document.getElementById('login').value;
     object.email = document.getElementById('inputEmail').value;
-    object.password = document.getElementById('inputPassword').value;
+    var str = document.getElementById('inputPassword').value;
+    object.password = str.hashCode();
     object.age = document.getElementById('age').value;
     object.name = document.getElementById('firstName').value;
 
     return object;
+}
+
+function validateRegistration(){
+    removeAllError();
+
+    var str = document.getElementById('login').value;
+    var parrent = /^[a-zA-z]{1}[a-zA-Z1-9]{3,20}/i;
+    if(!parrent.test(str)) VT.addClass(".help-login",'error');
+
+    str = document.getElementById('inputEmail').value;
+    parrent = /[0-9a-z_]+@[0-9a-z_]+\.[a-z]{2,5}/i;
+    if(!parrent.test(str)) VT.addClass('.help-email','error');
+
+    if((document.getElementById('inputPassword').value != document.getElementById('confirmPassword').value) ||
+        (document.getElementById('inputPassword').value.length < 5)) VT.addClass('.help-password','error');
+
+    if(VT.getAllEl('.error').length == 0) return true;
+    return false;
+}
+
+function removeAllError(){
+    var bool = true;
+    while(bool){
+        bool = VT.removeClass('.error', 'error');
+    }
 }
