e7a9563dbc142c200dcd76ff61115018a95f0023
tima1702 <tima1702@gmail.com>
2017-06-27 14:07:11 +0300

    реализованно изменеие данных пользователя

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ String.prototype.hashCode = function(){
     return hash;
 }
 
-
 function registration(form){
 
     if(!validateRegistration()) return false;
@@ -133,9 +132,7 @@ function updateLocalFromSession(){
     localStorage.setItem(session.login,userInfo);
 }
 
-
 function changeName(){
-
     VT.removeClass('.help-name','error');
 
     var name = document.getElementById('firstName').value;
@@ -148,4 +145,70 @@ function changeName(){
 
     sessionStorage.setItem('name',name);
     updateLocalFromSession();
+
+    document.getElementById('firstName').value = "";
+    alert("Имя измененно");
+}
+
+function changeAge(){
+    VT.removeClass('.help-age');
+
+    var age = document.getElementById('age').value;
+    age = Number(age);
+
+    if( age < 1 || age >120 ){
+        VT.addClass('.help-age','error');
+        return false;
+    }
+
+    sessionStorage.setItem('age',age);
+    updateLocalFromSession();
+
+
+    document.getElementById('age').value = "";
+    alert("Возраст изменен");
+}
+
+function changeEmail(){
+    VT.removeClass('.help-email','error');
+
+    var email = document.getElementById('inputEmail').value;
+    var parrent = /[0-9a-z_]+@[0-9a-z_]+\.[a-z]{2,5}/i;;
+
+    if(!parrent.test(email)){
+        VT.addClass('.help-email','error');
+        return false;
+    };
+
+    sessionStorage.setItem('email',email);
+    updateLocalFromSession();
+
+    document.getElementById('inputEmail').value = "";
+    alert("Почта измененна");
+}
+
+function changePassword(){
+    VT.removeClass('.help-password','error');
+
+    if((document.getElementById('inputPassword').value != document.getElementById('confirmPassword').value) ||
+        (document.getElementById('inputPassword').value.length < 5)) {
+        VT.addClass('.help-password', 'error');
+        return false;
+    }
+
+    var password = document.getElementById('inputPassword').value;
+    password = password.hashCode();
+
+    var login = sessionStorage.getItem('login');
+    var userInfo = localStorage.getItem(login);
+
+    userInfo = JSON.parse(userInfo);
+    userInfo.password = password;
+    userInfo = JSON.stringify(userInfo);
+
+    localStorage.setItem(login,userInfo);
+
+    document.getElementById('inputPassword').value = "";
+    document.getElementById('confirmPassword').value ="";
+    alert("Пароль изменнен");
 }
